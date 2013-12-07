a88aae981765d9baebdedd20633263e4a11f82fe
acflewis <acflewis@gmail.com>
2013-12-07 00:37:36 -0800

    herokufix2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,11 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @chimes = Chime.find_by_user_id(current_user.id) 
-    render :json => {
-        :chimes => @chimes,
-        :user => current_user
-     }
+    render :json => @chimes
+    # render :json => {
+    #     :chimes => @chimes,
+    #     :user => current_user
+    #  }
   end
   
   def index
