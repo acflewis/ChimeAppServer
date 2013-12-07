54159a702b2749baf0f2ba81a8475356ecbec37a
acflewis <acflewis@gmail.com>
2013-12-07 09:27:58 -0800

    FixHeroku2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @chimes = Chime.find_by_user_id(current_user.id) 
-    render :json => @user
+    redirect_to users_url
+    # render :json => @user
     # render :json => {
  #        :chimes => @chimes,
  #        :user => current_user
