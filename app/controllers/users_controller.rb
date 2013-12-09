1f68a31345f8e833d82370f9443e37c7d21726a5
acflewis <acflewis@gmail.com>
2013-12-08 19:54:57 -0800

    Change user show action 4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,11 @@ class UsersController < ApplicationController
     
     @user = current_user
     @chimes = Chime.find_all_by_user_id(current_user.id) 
-    render :json => {
-        :chimes => @chimes,
-        :user => current_user
-     }
+    render :json => @chimes
    else
      @user = User.find(2)
      @chimes = Chime.find_all_by_user_id(2) 
-     render :json => {
-         :chimes => @chimes,
-         :user => @user
-      }
+     render :json => @chimes
      # render :json => "Please Login"
    end
   end
